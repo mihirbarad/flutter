2188a6d2e465ba9a80bf35d8e784a4bca787d3dd
mihirsofttech <106421028+mihirsofttech@users.noreply.github.com>
2022-07-20 14:41:36 +0530

    onetime

---
+++ Class work/datatype.dart
@@ -0,0 +1,20 @@
+main() {
+  int num = 10;
+  String name = "aaa";
+  bool status = true;
+  double price = 10.2;
+  List shopping = ["vegas", "fruits"];
+  Map student = {
+    "name": "aaa",
+    "city": "bhavnagar",
+    "pin": 364002,
+  };
+
+  print(num);
+  print(name);
+  print(status);
+
+  print(price);
+  print(shopping);
+  print(student);
+}
